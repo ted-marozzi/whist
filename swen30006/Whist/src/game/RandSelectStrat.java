791d3e9b9ec4b991ab59297d70dae87b94fad698
Fei Yuan <yuanf3@student.unimelb.edu.au>
2020-11-02 17:13:36 +1100

    use seed for random select

--- swen30006/Whist/src/game/RandSelectStrat.java
+++ swen30006/Whist/src/game/RandSelectStrat.java
@@ -8,12 +8,11 @@ import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class RandSelectStrat implements ISelectStrat {
-    static final Random random = ThreadLocalRandom.current();
     // return random Card from ArrayList
     
     @Override
     public Card select(Hand hand) {
-        int x = random.nextInt(hand.getNumberOfCards());
+        int x = Whist.random.nextInt(hand.getNumberOfCards());
         return hand.get(x);
     }
 }
